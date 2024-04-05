fa7551ad77d4e6a7b8905ef4147ec2355e544b77
Rajesh-Reel <rajreel2@hotmail.com>
2024-04-05 20:59:20 +0100

    values added for coinLevels variable as per disney post

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log("hello");
+
+//https://disneyspeedstorm.com/news/disney-speedstorm-community-pit-stop-upgrade-system-rework
+const coinLevels = [0, 150, 170, 190, 210, 230, 250, 270, 300, 320, 350, 380, 400, 450, 500, 540, 580, 630, 700, 750, 820, 900, 970, 1050, 1150, 1250, 1350, 1450, 1550, 1700, 1850, 2000, 2150, 2300, 2500, 2700, 3000, 3300, 3600, 4000, 4300, 4750, 5200, 5700, 6200, 6700, 7300, 8000, 8900, 10000];
+const racerShards = [];
